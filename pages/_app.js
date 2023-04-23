a0f649780f217ff37a373aadc3f97f222c6a9bb0
Nedim Begic <nedim.begic93@gmail.com>
2023-04-23 22:02:51 +0200

    change font family

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,12 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Beau+Rivage&family=Grape+Nuts&family=Montserrat:ital,wght@1,100&family=Roboto+Serif:opsz,wght@8..144,100&family=Roboto:wght@100;300&family=Rubik+Microbe&family=Rubik+Moonrocks&family=Rubik+Wet+Paint&display=swap"
+          rel="stylesheet"
+        />
         <title>Share Recipes</title>
         <meta
           name="description"
